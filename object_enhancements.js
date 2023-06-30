e3bcb174e239be962930afb5bde881d3c779e655
eduardopedroza <131719448+eduardopedroza@users.noreply.github.com>
2023-06-30 16:07:09 -0700

    Add files via upload

---
+++ object_enhancements.js
@@ -0,0 +1,72 @@
+function createInstructor(firstName, lastName){
+    return {
+      firstName: firstName,
+      lastName: lastName
+    }
+  }
+
+  /* Write an ES2015 Version */
+
+  const createInstructor = (firstName, lastName) => {
+    return {
+        firstName,
+        lastName
+    }
+  }
+
+
+
+
+
+var favoriteNumber = 42;
+
+var instructor = {
+  firstName: "Colt"
+}
+
+instructor[favoriteNumber] = "That is my favorite!"
+
+
+/* Write an ES2015 Version */
+
+let favorite = 42;
+const instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite"
+}
+
+
+
+
+
+var instructor = {
+    firstName: "Colt",
+    sayHi: function(){
+      return "Hi!";
+    },
+    sayBye: function(){
+      return this.firstName + " says bye!";
+    }
+  }
+
+  /* Write an ES2015 Version */
+
+  const instructor = {
+    firstName: "Colt",
+    sayHi() {
+        return "Hi";
+    },
+    sayBye() {
+        return this.firstName + " says bye!";
+    }
+  }
+
+  const createAnimal = (species, verb, noise) => {
+    return = {
+        species, 
+        [verb](){
+            return noise;
+        }
+    }
+  }
+
